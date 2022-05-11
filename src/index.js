6c6b678a82d16a0c1491216e6ad1d867eda6f6a7
Vinay Uttam Vemparala <15381417+vinayuttam@users.noreply.github.com>
2022-05-11 11:20:27 +0530

    Test environment variables

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+console.log(process.env.REACT_APP_API_URL);
 root.render(
   <React.StrictMode>
     <App />
